3f775f35697a828d23c7bc3baa0f707a79b93a10
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-18 23:09:10 +0700

    Update server.port

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
     }
-  }
+  },
+  server: {
+    port: 8080,
+  },
 })
