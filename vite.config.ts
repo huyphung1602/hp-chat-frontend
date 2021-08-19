4bead34e33c51303c9be1386f2803b1c523f4db6
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-19 18:35:19 +0700

    Remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,5 @@ export default defineConfig({
   },
   server: {
     port: 8080,
-    proxy: {
-      // string shorthand
-      "/messages": "http://localhost:3000/api/v1/",
-      "/sign_up": "http://localhost:3000/api/v1/",
-    }
   },
 })
