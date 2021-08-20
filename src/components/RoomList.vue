6bde8d66f5bf72a6703521d134764b263ac62e77
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-21 00:57:21 +0700

    Update css for the stucture of chat application

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -1,20 +1,30 @@
 <template>
-  <div class="container mx-auto flex-col mt-20">
-    <p class="text-4xl">Rooms</p>
-    <div
-      class="box-border h-auto max-w-md p-2 border-4 my-4"
-      v-for="room in rooms"
-      :key="room.id"
-    >
-      <div
-        class="room-name pb-2"
-      >
-        Room: {{ room.name }}
+  <div class="container mx-auto px-6">
+    <div class="grid grid-cols-12 gap-4 h-screen p-10">
+      <div class="col-span-4">
+        <div
+          class="box-border p-2 border-4 my-4"
+          v-for="room in rooms"
+          :key="room.id"
+        >
+          <div
+            class="room-name pb-2"
+          >
+            Room: {{ room.name }}
+          </div>
+          <div
+            class="room-owner"
+          >
+            Owner: {{ room.owner }}
+          </div>
+        </div>
       </div>
-      <div
-        class="room-owner"
-      >
-        Owner: {{ room.owner }}
+      <div class="col-span-8">
+        <div
+          class="box-border h-full my-4"
+        >
+          <router-view />
+        </div>
       </div>
     </div>
   </div>
