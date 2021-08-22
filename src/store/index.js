a474bae03a3a9c53f0141f3a0324bc2321ec46f3
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-22 19:34:10 +0700

    Add draft Vuex store

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'vuex'
+
+const messages = {
+  state: () => ({
+
+  }),
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  },
+}
+
+const store = createStore({
+  modules: {
+    messages,
+  }
+})
+
+export default store;
