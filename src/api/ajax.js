4b5b767273af50b195e79300310282e00407c8df
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-28 03:39:54 +0700

    Add vue-cookie, use it for the CSRF token from Vue to Rails

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import cookie from 'vue-cookie'
 
 // axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-CSRF-Token'] = cookie.get('CSRF-TOKEN');
 
 export const standardGet = async (path, params = {}, headers = {}) => {
   const { data } = await axios.get(path, { params, headers });
