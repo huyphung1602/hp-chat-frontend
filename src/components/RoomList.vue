2f87c374e16f765b798ce6e7fa66b791f0a65f87
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-28 18:58:12 +0700

    Add logic to show active room

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -2,17 +2,18 @@
   <div class="grid grid-cols-12 gap-4 h-screen p-10 bg-gray-50">
     <div class="col-span-2">
       <div
-        class="border border-gray-200 shadow rounded-md p-2 mb-5"
+        class="border border-gray-200 shadow rounded-md p-4 mb-5"
+        :class="{'active-room': activeRoomId === room.id }"
         v-for="room in rooms"
         :key="room.id"
         @click.prevent="fetchRoomMessages(room.id)"
       >
         <router-link :to="{ name: 'room', params: { id: room.id } }">
           <div class="font-normal pb-2">
-            Room: {{ room.name }}
+            {{ room.name }}
           </div>
           <div class="font-semibold">
-            Owner: {{ room.owner }}
+            Owner: {{ room.owner.name }}
           </div>
         </router-link>
       </div>
@@ -32,15 +33,18 @@ import { each } from 'lodash';
 import { onMounted, ref } from 'vue';
 import consumer from '@/api/consumer.ts';
 import { fetchRooms } from '@/api/roomApi.ts';
-import { useStore } from 'vuex'
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
 
 export default {
   name: 'RoomList',
   setup() {
     const rooms = ref([]);
+    const activeRoomId = ref();
+    const router = useRouter();
+
     const subscribeWebSocket = room => consumer.subscriptions.create({ channel: 'RoomChannel', room_id: room.id }, {
       received(data) {
-        console.log(data);
         store.commit('appendMessage', {
           roomId: room.id,
           message: data,
@@ -49,18 +53,38 @@ export default {
     });
     
     const store = useStore()
-    const fetchRoomMessages = roomId => store.dispatch('fetchMessages', roomId);
+    const fetchRoomMessages = roomId => {
+      store.dispatch('fetchMessages', roomId);
+      activeRoomId.value = roomId;
+    };
 
     onMounted(async () => {
       const data = await fetchRooms();
       rooms.value = data;
+
+      if (data.length !== 0) {
+        // Fetch default active room
+        const defaultRoomId = data[0].id;
+        activeRoomId.value = defaultRoomId;
+        store.dispatch('fetchMessages', defaultRoomId);
+        router.push({ name: 'room', params: { id: defaultRoomId }});
+      }
+
       each(data, room => subscribeWebSocket(room));
     });
 
     return {
       rooms,
       fetchRoomMessages,
+      activeRoomId,
     };
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .active-room {
+    --tw-border-opacity: 1;
+    border-left: 4px solid rgba(67, 56, 202, var(--tw-border-opacity));
+  }
+</style>
