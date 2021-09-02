4ac44e51f3232f4c4d6ade398a3f90e6cf2d2ed4
huyphung1602 <huy.phung.sw@gmail.com>
2021-09-03 03:54:15 +0700

    Add copy the invitation link to clipboard button]

--- src/components/modals/InvitationGenerationModal.vue
+++ src/components/modals/InvitationGenerationModal.vue
@@ -8,13 +8,24 @@
       </div>
     </template>
     <template v-slot:body>
-      <input
-        v-model="invitationUrl"
-        readonly
-        type="text"
-        placeholder="Type your room name"
-        class="p-1 text-sm font-normal w-full border rounded-md focus:outline-none focus:ring focus:border-blue-400"
-      />
+      <div class="flex flex-row items-center justify-between">
+        <input
+          v-model="invitationUrl"
+          readonly
+          type="text"
+          placeholder="Type your room name"
+          class="p-1 text-sm font-normal w-full border rounded-md focus:outline-none focus:ring focus:border-blue-400"
+        />
+        <button
+          class="rounded-md bg-gray-200 text-gray-500 hover:bg-gray-300 focus::bg-gray-400 focus:outline-none p-2 ml-2"
+          type="button"
+          @click="copyToClipBoard"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+          </svg>
+        </button>
+      </div>
     </template>
     <template v-slot:footer>
       <button
@@ -49,9 +60,13 @@
     setup(props, { emit }) {
       const close = () => emit('close');
       const invitationUrl = computed(() => props.invitationUrl);
+      const copyToClipBoard = () => {
+        navigator.clipboard.writeText(invitationUrl.value);
+      };
 
       return {
         invitationUrl,
+        copyToClipBoard,
         close,
       };
     }
